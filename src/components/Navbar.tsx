825c4d9465b7d5bf39b64b925505ca03a4b6442e
Joseph Asegid <josephasegid@gmail.com>
2025-06-15 15:41:42 +0300

    IMAGE

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
           <Link href="/" className="flex items-center space-x-2">
           <div className="relative w-10 h-10 flex items-center justify-center bg-red-700 rounded-md overflow-hidden">
   <img
-    src="src\assets\logo.jpg"
+    src="src/assets/logo.jpg"
     alt="Logo"
     className="w-12 h-12 object-contain"
   />
